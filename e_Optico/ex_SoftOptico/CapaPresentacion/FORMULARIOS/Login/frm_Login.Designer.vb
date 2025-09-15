db42ee4bb63c954556dacafecba0e2eee41c5196
Wilmer Flores <wopticaatlantico@gmail.com>
2025-09-15 08:54:59 -0400

    add
    add

--- e_Optico/ex_SoftOptico/CapaPresentacion/FORMULARIOS/Login/frm_Login.Designer.vb
+++ e_Optico/ex_SoftOptico/CapaPresentacion/FORMULARIOS/Login/frm_Login.Designer.vb
@@ -22,6 +22,7 @@ Partial Class frm_Login
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_Login))
         pnlContenido = New Panel()
         btnOlvidePass = New ButtonLinkUI()
         btnAceptar = New CommandButtonUI()
@@ -29,12 +30,18 @@ Partial Class frm_Login
         btnMinimizar = New FontAwesome.Sharp.IconButton()
         txtPass = New TextBoxLabelUI()
         txtUsuario = New TextBoxLabelUI()
+        TextOnlyTextBoxLabelui1 = New TextOnlyTextBoxLabelUI()
+        PasswordTextBoxLabelui1 = New PasswordTextBoxLabelUI()
+        Comboui1 = New ComboUI()
         pnlContenido.SuspendLayout()
         SuspendLayout()
         ' 
         ' pnlContenido
         ' 
-        pnlContenido.BackgroundImage = My.Resources.Resources.Pass2
+        pnlContenido.BackgroundImage = CType(resources.GetObject("pnlContenido.BackgroundImage"), Image)
+        pnlContenido.Controls.Add(Comboui1)
+        pnlContenido.Controls.Add(PasswordTextBoxLabelui1)
+        pnlContenido.Controls.Add(TextOnlyTextBoxLabelui1)
         pnlContenido.Controls.Add(btnOlvidePass)
         pnlContenido.Controls.Add(btnAceptar)
         pnlContenido.Controls.Add(btnSalir)
@@ -54,10 +61,10 @@ Partial Class frm_Login
         btnOlvidePass.Font = New Font("Tahoma", 6F)
         btnOlvidePass.HoverColor = Color.DodgerBlue
         btnOlvidePass.LinkText = "Olvide mi contraseña"
-        btnOlvidePass.Location = New Point(629, 457)
+        btnOlvidePass.Location = New Point(634, 421)
         btnOlvidePass.Margin = New Padding(2)
         btnOlvidePass.Name = "btnOlvidePass"
-        btnOlvidePass.NormalColor = Color.WhiteSmoke
+        btnOlvidePass.NormalColor = Color.Green
         btnOlvidePass.Size = New Size(166, 24)
         btnOlvidePass.SubrayarAlPasar = True
         btnOlvidePass.TabIndex = 4
@@ -74,9 +81,9 @@ Partial Class frm_Login
         btnAceptar.EstiloBoton = CommandButtonUI.EstiloBootstrap.Success
         btnAceptar.Font = New Font("Century Gothic", 10F, FontStyle.Bold)
         btnAceptar.Icono = FontAwesome.Sharp.IconChar.CheckCircle
-        btnAceptar.Location = New Point(574, 489)
+        btnAceptar.Location = New Point(583, 464)
         btnAceptar.Name = "btnAceptar"
-        btnAceptar.RadioBorde = 8
+        btnAceptar.RadioBorde = 12
         btnAceptar.Size = New Size(266, 42)
         btnAceptar.TabIndex = 2
         btnAceptar.Text = "CommandButtonui1"
@@ -126,7 +133,7 @@ Partial Class frm_Login
         txtPass.IconoDerechoChar = FontAwesome.Sharp.IconChar.Fingerprint
         txtPass.LabelColor = Color.WhiteSmoke
         txtPass.LabelText = "Contraseña"
-        txtPass.Location = New Point(558, 353)
+        txtPass.Location = New Point(231, 189)
         txtPass.MensajeError = "La contraseña no puede quedar vacia..."
         txtPass.Name = "txtPass"
         txtPass.PaddingAll = 10
@@ -156,7 +163,7 @@ Partial Class frm_Login
         txtUsuario.IconoDerechoChar = FontAwesome.Sharp.IconChar.UserCheck
         txtUsuario.LabelColor = Color.WhiteSmoke
         txtUsuario.LabelText = "Usuario"
-        txtUsuario.Location = New Point(558, 247)
+        txtUsuario.Location = New Point(231, 67)
         txtUsuario.MensajeError = "Usuario desconocido..."
         txtUsuario.Name = "txtUsuario"
         txtUsuario.PaddingAll = 10
@@ -168,6 +175,63 @@ Partial Class frm_Login
         txtUsuario.TextoUsuario = ""
         txtUsuario.UsarModoContraseña = False
         txtUsuario.ValidarComoCorreo = False
+        ' 
+        ' TextOnlyTextBoxLabelui1
+        ' 
+        TextOnlyTextBoxLabelui1.BackColor = Color.Transparent
+        TextOnlyTextBoxLabelui1.CampoRequerido = False
+        TextOnlyTextBoxLabelui1.CapitalizarTexto = False
+        TextOnlyTextBoxLabelui1.CapitalizarTodasLasPalabras = False
+        TextOnlyTextBoxLabelui1.ColorTitulo = Color.DarkSlateGray
+        TextOnlyTextBoxLabelui1.IconoColor = Color.FromArgb(CByte(57), CByte(103), CByte(208))
+        TextOnlyTextBoxLabelui1.IconoDerechoChar = FontAwesome.Sharp.IconChar.UserCheck
+        TextOnlyTextBoxLabelui1.Location = New Point(559, 142)
+        TextOnlyTextBoxLabelui1.MaxCaracteres = 0
+        TextOnlyTextBoxLabelui1.MensajeError = "Campo requerido"
+        TextOnlyTextBoxLabelui1.Name = "TextOnlyTextBoxLabelui1"
+        TextOnlyTextBoxLabelui1.PaddingIzquierda = 8
+        TextOnlyTextBoxLabelui1.PaddingIzquierdaIcono = 10
+        TextOnlyTextBoxLabelui1.Placeholder = "Ingrese datos"
+        TextOnlyTextBoxLabelui1.PlaceholderColor = Color.Gray
+        TextOnlyTextBoxLabelui1.Size = New Size(308, 74)
+        TextOnlyTextBoxLabelui1.TabIndex = 5
+        TextOnlyTextBoxLabelui1.TextoLabel = "Usuario:"
+        ' 
+        ' PasswordTextBoxLabelui1
+        ' 
+        PasswordTextBoxLabelui1.BackColor = Color.Transparent
+        PasswordTextBoxLabelui1.CampoRequerido = False
+        PasswordTextBoxLabelui1.CapitalizarTexto = False
+        PasswordTextBoxLabelui1.CapitalizarTodasLasPalabras = False
+        PasswordTextBoxLabelui1.ColorTitulo = Color.DarkSlateGray
+        PasswordTextBoxLabelui1.IconoColor = Color.FromArgb(CByte(57), CByte(103), CByte(208))
+        PasswordTextBoxLabelui1.IconoDerechoChar = FontAwesome.Sharp.IconChar.Fingerprint
+        PasswordTextBoxLabelui1.Location = New Point(558, 218)
+        PasswordTextBoxLabelui1.MaxCaracteres = 0
+        PasswordTextBoxLabelui1.MensajeError = "Campo requerido"
+        PasswordTextBoxLabelui1.Name = "PasswordTextBoxLabelui1"
+        PasswordTextBoxLabelui1.PaddingIzquierda = 8
+        PasswordTextBoxLabelui1.PaddingIzquierdaIcono = 10
+        PasswordTextBoxLabelui1.Placeholder = "Ingrese contraseña"
+        PasswordTextBoxLabelui1.PlaceholderColor = Color.Gray
+        PasswordTextBoxLabelui1.Size = New Size(309, 72)
+        PasswordTextBoxLabelui1.TabIndex = 6
+        PasswordTextBoxLabelui1.TextoLabel = "Contraseña:"
+        ' 
+        ' Comboui1
+        ' 
+        Comboui1.BackColor = Color.Transparent
+        Comboui1.CampoRequerido = False
+        Comboui1.ColorTitulo = Color.DarkSlateGray
+        Comboui1.Location = New Point(559, 296)
+        Comboui1.MensajeError = "Este campo es requerido"
+        Comboui1.MostrarError = False
+        Comboui1.Name = "Comboui1"
+        Comboui1.Placeholder = "Selecciones una Opcion..."
+        Comboui1.PlaceholderColor = Color.Gray
+        Comboui1.Size = New Size(308, 76)
+        Comboui1.TabIndex = 7
+        Comboui1.TextoLabel = "Local:"
         ' 
         ' frm_Login
         ' 
@@ -193,4 +257,7 @@ Partial Class frm_Login
     Friend WithEvents btnSalir As FontAwesome.Sharp.IconButton
     Friend WithEvents btnAceptar As CommandButtonUI
     Friend WithEvents btnOlvidePass As ButtonLinkUI
+    Friend WithEvents PasswordTextBoxLabelui1 As PasswordTextBoxLabelUI
+    Friend WithEvents TextOnlyTextBoxLabelui1 As TextOnlyTextBoxLabelUI
+    Friend WithEvents Comboui1 As ComboUI
 End Class
